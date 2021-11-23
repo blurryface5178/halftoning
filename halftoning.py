910081ac3e5799938311a06dda3437e00082023f
blurryface5178 <abhijanwasti@gmail.com>
2021-11-23 16:09:59 -0500

    Progress with fs, working on dithering

--- halftoning.py
+++ halftoning.py
@@ -1,26 +1,26 @@
-import numpy as np
 import cv2
-from PIL import Image
 from math import floor
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class Halftoning:
     def __init__(self, image, method = 'fs'):
         self.image = image.copy()
         if method == 'fs':
             self.method = self.floyd_steinberg
+        elif method == 'dt':
+            self.method = self.noise
         else:
-            self.method = self.dither
+            print('Not a known method')
         self.output = image.copy()
 
     def normalized(self, pix, min=0, max=255):
         f = ((pix - min) / (max - min)) - 0.5
         return f
 
-    def threshold(self, pix, thresh=128):
-        return 255 * floor(pix / thresh)
-        # if pix>=thresh:
-        #     return 1
-        # return 0
+    def threshold(self, pix, thresh=127):
+        return 255 * floor(pix / (thresh))
 
     def floyd_steinberg(self, resize=None, min=0, max=255):
         if isinstance(resize, tuple):
@@ -30,15 +30,11 @@ class Halftoning:
         
         w, h = output.shape
         
-        for j in range(1, h):
-            for i in range(1, w):
-                # fq = self.threshold(self.normalized(output[i][j]))
-                # err = output[i][j] - fq*(max - min) - min 
-
-                pix = output[i][j]
-                new_pix = self.threshold(pix)
-                output[i][j] = new_pix
-                err = pix - new_pix
+        for j in range(0, h):
+            for i in range(0, w):
+                fq = self.threshold(output[i][j])
+                err = output[i][j] - fq * (max - min) - min 
+                output[i][j] = fq
 
                 if(i < w-1):
                     output[i+1][j  ] += round(err * 7/16)
@@ -49,19 +45,36 @@ class Halftoning:
                 if(i < w-1 and j < h-1):
                     output[i+1][j+1] += round(err * 1/16)
 
-                # output[i][j] = self.threshold(output[i][j], 127) * (max-min) + min
-        self.output = output.copy()
+        self.output = output
 
-    def dither(self, min=0, max=255):
-        w, h= self.image.shape
+    def noise(self, resize=None, min=0, max=255):
+        if isinstance(resize, tuple):
+            output = cv2.resize(self.output, resize)
+        else:
+            output = self.output.copy()
+        
+        w, h= output.shape
 
-        noise = np.random.random(size=(w, h))
-        output = self.normalized(self.image)
-        output = cv2.add(output, noise)
+        noise = np.random.uniform(-np.pi, np.pi, size=(w, h))
+        plt.imshow(noise)
 
-        for j in range(h-1):
-            for i in range(1, w-1):
-                output[i][j] = self.threshold(output[i][j], 1)
+        for x in range(w):
+            for y in range(h):
+                if (x-w/2)**2+(y-h/2)**2 < 10000:
+                    noise[x][y] /= 3.14
+        plt.figure()
+        plt.imshow(noise)
+
+        idft_noise = np.fft.ifft2(noise)
+        mask = idft_noise.real # - min / (max - min)
+        output = output - min / (max - min)
+
+        plt.figure()
+        plt.imshow(np.absolute(idft_noise))
+
+        plt.show()
+        output = cv2.add(output, mask, dtype=cv2.CV_8U)
+        _, output = cv2.threshold(output, 127, 255, cv2.THRESH_BINARY)
 
         self.output = output.copy()
     
@@ -81,37 +94,14 @@ class Halftoning:
     def save(self, filename='output'):
         cv2.imwrite(filename, self.output)
 
-def color_fs(image):
+def color_fs(image, resize = None):
         b, g, r = cv2.split(image)
         
         fs_red = Halftoning(r, 'fs')
-        fs_red.run((128, 128))
+        fs_red.run(resize)
         fs_green = Halftoning(g, 'fs')
-        fs_green.run((128, 128))
+        fs_green.run(resize)
         fs_blue = Halftoning(b, 'fs')
-        fs_blue.run((128, 128))
+        fs_blue.run(resize)
     
         return cv2.merge([fs_blue.output, fs_green.output, fs_red.output]) 
-
-def main():
-    filename = 'abe.jpg'
-    image = cv2.imread(filename)
-    image_bw = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
-    floyd_steinberg = Halftoning(image_bw, 'fs')
-    floyd_steinberg.run()
-    floyd_steinberg.show()
-    floyd_steinberg.save('fs'+filename)
-
-    # output = color_fs(image)
-    # cv2.imshow('Output', output)
-    # cv2.imwrite('fs_color_'+filename, output)
-
-    while(cv2.waitKey(0) != 27):
-        continue
-
-    cv2.destroyAllWindows()
-    pass
-
-if __name__=='__main__':
-    main()
---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+from Halftoning import Halftoning
+
+def main():
+    filename = 'cat_hw.jpg'
+    image_bw = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+    dithering = Halftoning(image_bw, 'fs')
+    dithering.run()
+    dithering.show()
+
+    while(cv2.waitKey(0) != 27):
+        continue
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
